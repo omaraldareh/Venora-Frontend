24b7c7b0a1b0b46a784f7c3edd1ed2b796fd7368
omaraldareh <aldarehomar@gmail.com>
2026-06-04 23:52:42 +0300

    fix: match import path with filename otp in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ChooseAccount from './pages/ChooseAccount';
-import Otp from './pages/Otp';
+import Otp from './pages/otp';
 import ForgetPassword from './pages/ForgetPassword';
 import ResetPassword from './pages/ResetPassword';
 import Browse from './pages/Browse';
